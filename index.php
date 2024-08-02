41cf456e5fcb284dbd434e3f3acfa4d1b9992602
Duveimar <72568140+Duveimar@users.noreply.github.com>
2024-08-02 14:32:40 -0500

    PLantilla base html

--- index.php
+++ index.php
@@ -2,32 +2,49 @@
 
     require_once "./config/app.php";
     require_once "./autoload.php";
- /*---------- Iniciando sesion ----------*/
- require_once "./app/views/inc/session_start.php";
 
- if(isset($_GET['views'])){
-     $url=explode("/", $_GET['views']);
- }else{
-     $url=["login"];
- }
+    /*---------- Iniciando sesion ----------*/
+    require_once "./app/views/inc/session_start.php";
 
+    if(isset($_GET['views'])){
+        $url=explode("/", $_GET['views']);
+    }else{
+        $url=["login"];
+    }
 
+?>
 
-/*---------- Plantilla base
-obtener la varieble tipo get views
-condidisonal doble 
-Funcion explode () partir un string 
-----------*/
-
-    ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php require_once "./app/views/inc/head.php"; ?>
 </head>
 <body>
-    
+    <?php
+        use app\controllers\viewsController;
+        use app\controllers\loginController;
+
+        $insLogin = new loginController();
+
+        $viewsController= new viewsController();
+        $vista=$viewsController->obtenerVistasControlador($url[0]);
+
+        if($vista=="login" || $vista=="404"){
+            require_once "./app/views/content/".$vista."-view.php";
+        }else{
+
+            # Cerrar sesion #
+            if((!isset($_SESSION['id']) || $_SESSION['id']=="") || (!isset($_SESSION['usuario']) || $_SESSION['usuario']=="")){
+                $insLogin->cerrarSesionControlador();
+                exit();
+            }
+
+            require_once "./app/views/inc/navbar.php";
+
+            require_once $vista;
+        }
+
+        require_once "./app/views/inc/script.php"; 
+    ?>
 </body>
 </html>
